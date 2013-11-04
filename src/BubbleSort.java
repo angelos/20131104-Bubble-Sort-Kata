a8b7c85f0e8c6bd76e9be951fe9842da638b3ecf
Angelo van der Sijpt <angelo.vandersijpt@luminis.eu>
2013-11-04 18:55:07 +0100

    Initial, as shown on http://www.youtube.com/watch?v=AraZURpk_YA

---
+++ src/BubbleSort.java
@@ -0,0 +1,20 @@
+
+public class BubbleSort {
+	
+	public static int[] sort(int[] array) {
+		boolean swapped = true;
+		while(swapped) {
+			for(int i = 1; i < array.length; i++) {
+				swapped = false;
+				if (array[i - 1] > array[i]) {
+					int temp = array[i];
+					array[i] = array[i - 1];
+					array[i - 1] = temp;
+					swapped = true;
+				}
+			}
+		}
+		return array;
+	}
+
+}
---
+++ tests/BubbleSortTests.java
@@ -0,0 +1,31 @@
+import static org.junit.Assert.assertArrayEquals;
+
+import org.junit.Test;
+
+
+public class BubbleSortTests {
+
+	@Test
+	public void sortSortedList() {
+		checkBubbleSortedArrayReturnsExpected(new int[] {0, 1}, new int[] {0, 1});
+	}
+	
+	@Test
+	public void sortUnsortedListOfLengthTwo() throws Exception {
+		checkBubbleSortedArrayReturnsExpected(new int[] {0,  1}, new int[] {1, 0});
+	}
+	
+	@Test
+	public void sortPartiallyUnsortedArrayOfLengthThree() {
+		checkBubbleSortedArrayReturnsExpected(new int[] {0,  1, 2}, new int[] {0, 2, 1});
+	}
+	
+	@Test
+	public void sortUnsortedArrayOfLengthThree() {
+		checkBubbleSortedArrayReturnsExpected(new int[]{0, 1, 2}, new int[]{2, 1, 0});
+	}
+
+	private void checkBubbleSortedArrayReturnsExpected(int[] expectedArray, int[] inputArray) {
+		assertArrayEquals(expectedArray, BubbleSort.sort(inputArray));
+	}
+}
